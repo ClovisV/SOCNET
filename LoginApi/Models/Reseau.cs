b2bfc50a74c1ac722ce41a23511c872e69842773
Clovis Janicot-Tixier <ig01205@etu.parisdescartes.fr>
2017-04-30 22:36:56 +0200

    Mise à jour des fichiers en lien avec Facebook et des références utilisées

--- LoginApi/Models/Reseau.cs
+++ LoginApi/Models/Reseau.cs
@@ -7,15 +7,17 @@ namespace LoginApi.Models
     {
         public int reseauId { get; set; }
         public string reseauSocial { get; set; }
+        public string reseauConnexionId { get; set; }
 
         private Random r = new Random(1);
         private const int minValueReseau = 1;
         private const int maxValueReseau = 10;
 
-        public Reseau(string reseauSocial)
+        public Reseau(string reseauSocial, string reseauConnexionId)
         {
             reseauId = r.Next(minValueReseau, maxValueReseau);
             this.reseauSocial = reseauSocial;
+            this.reseauConnexionId = reseauConnexionId;
         }
     }
 }
